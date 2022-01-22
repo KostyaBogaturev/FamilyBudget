9df68d49c0f1caf170a01f68b1b40a3825d50344
Kostya-Bog <59747320+Kostya-Bog@users.noreply.github.com>
2022-01-22 16:10:08 +0200

    create data access layer

---
+++ Repository/Contracts/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Repository.Contracts
+{
+    public interface IRepository<T>
+        where T: class
+    {
+        public Task<T> GetAsync(Guid id);
+
+        public Task<IEnumerable<T>> GetAllAsync();
+
+        public Task Create(T item);
+
+        public Task Delete(Guid id);
+    }
+}
---
+++ Repository/Entities/Family.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Repository.Entities
+{
+    class Family
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Budget { get; set; }
+
+        public IEnumerable<Guid> UserId { get; set; }
+    }
+}
